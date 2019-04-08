7e3063cf9dc5dfdcd4e77bc84141c6da9df50104
sayler666 <lukasz.chromy@gmail.com>
2019-04-08 09:55:47 +0200

    Update state

--- app/src/main/java/com/sayler/mvrx/ui/main/MainFragment.kt
+++ app/src/main/java/com/sayler/mvrx/ui/main/MainFragment.kt
@@ -10,19 +10,22 @@ import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import com.sayler.mvrx.R
 import kotlinx.android.synthetic.main.main_fragment.*
-import kotlin.random.Random
 
 data class HelloWorldState(
-    val title: String = "HelloWorld ${Random.nextInt(100)}"
-) : MvRxState
+    val title: String = "HelloWorld",
+    val count: Int = 0
+) : MvRxState {
+    var titleWithCount = "$title $count"
+}
 
-class HelloWorldViewModel(initialState: HelloWorldState) : MvRxViewModel<HelloWorldState>(initialState)
+class HelloWorldViewModel(initialState: HelloWorldState) : MvRxViewModel<HelloWorldState>(initialState) {
+    fun incrementCount() = setState { copy(count = count + 1) }
+}
 
 class MainFragment : BaseMvRxFragment() {
     private val viewModel: HelloWorldViewModel by fragmentViewModel()
 
     companion object {
-
         fun newInstance(): MainFragment = MainFragment()
     }
 
@@ -33,8 +36,14 @@ class MainFragment : BaseMvRxFragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        text.setOnClickListener {
+            viewModel.incrementCount()
+        }
+    }
+
     override fun invalidate() = withState(viewModel) {
-        text.text = it.title
+        text.text = it.titleWithCount
     }
 
 }
