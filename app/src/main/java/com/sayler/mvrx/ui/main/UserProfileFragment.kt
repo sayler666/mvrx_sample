a560b4bdfc445ae1a345b9dd73a1aa3d480d8e8d
sayler666 <lukasz.chromy@gmail.com>
2019-04-08 13:09:10 +0200

    Initialize state from args 2

--- app/src/main/java/com/sayler/mvrx/ui/main/UserProfileFragment.kt
+++ app/src/main/java/com/sayler/mvrx/ui/main/UserProfileFragment.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.airbnb.mvrx.BaseMvRxFragment
+import com.airbnb.mvrx.*
 import com.sayler.mvrx.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
+data class UserProfileState(val userId: String) : MvRxState
+
+class UserProfileViewModel(initialState: UserProfileState) : MvRxViewModel<UserProfileState>(initialState) {
+    companion object : MvRxViewModelFactory<UserProfileViewModel, UserProfileState> {
+        override fun initialState(viewModelContext: ViewModelContext): UserProfileState? {
+            val userId = (viewModelContext as FragmentViewModelContext).fragment<UserProfileFragment>().getUserId()
+            return UserProfileState(userId)
+        }
+    }
+}
 
 class UserProfileFragment : BaseMvRxFragment() {
+
     companion object {
         private const val ARG_USER_ID = "user_id"
         fun newInstance(userId: String): UserProfileFragment =
             UserProfileFragment().apply { arguments = Bundle().apply { putString(ARG_USER_ID, userId) } }
     }
+
+    private val viewModel: UserProfileViewModel by fragmentViewModel()
 
     fun getUserId() = arguments?.getString(ARG_USER_ID) ?: throw IllegalStateException("UserId missing!")
 
@@ -23,8 +38,8 @@ class UserProfileFragment : BaseMvRxFragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun invalidate() {
-
+    override fun invalidate() = withState(viewModel) { state ->
+        text.text = state.userId
     }
 
 }
